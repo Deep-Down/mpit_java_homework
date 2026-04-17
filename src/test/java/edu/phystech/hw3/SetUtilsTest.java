043ab3c91ded524c6f00ac4c94542baaa090d2fc
sierra <howwel766@gmail.com>
2026-04-17 19:47:28 +0800

    add set utility methods for union, intersection, difference, and symmetric difference

--- src/test/java/edu/phystech/hw3/SetUtilsTest.java
+++ src/test/java/edu/phystech/hw3/SetUtilsTest.java
@@ -1,12 +1,39 @@
 package edu.phystech.hw3;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SetUtilsTest {
+
+    public static <T> Set<T> union(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.addAll(second);
+        return result;
+    }
+
+    public static <T> Set<T> intersection(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.retainAll(second);
+        return result;
+    }
+
+    public static <T> Set<T> difference(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.removeAll(second);
+        return result;
+    }
+
+    public static <T> Set<T> symmetricDifference(Set<T> first, Set<T> second) {
+        Set<T> union = union(first, second);
+        Set<T> intersection = intersection(first, second);
+        
+        union.removeAll(intersection);
+        return union;
+    }
 
     @Test
     void unionTest() {
